8bc836a4258d87bafa34bc14265b856f0c2bb51d
Pang <tikumpornpang@gmail.com>
2021-06-13 16:59:06 +0700

    2

---
+++ ข้อ2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Final_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number = int.Parse(Console.ReadLine());
+            int[] inputNum = new int[Number];
+            for(int i = 0; i < inputNum.Length; i++)
+            {
+                inputNum[i] = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
