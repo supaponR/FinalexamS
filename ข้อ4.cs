ca2bfd8cf5bdd03f34c4b82d3166ae610844105c
Pang <tikumpornpang@gmail.com>
2021-06-13 16:59:15 +0700

    4

---
+++ ข้อ4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Final_4
+{
+    class Program
+    {
+        struct infoCity
+        {
+            public String NameCity;
+            public int Citynearly;
+            public int CityNearNum;
+        }
+        static void Main(string[] args)
+        {
+            int CityNum = int.Parse(Console.ReadLine());
+            infoCity[] CityNumArray = new infoCity[CityNum];
+            for (int i = 0; i < CityNum; i++)
+            {
+                CityNumArray[i].NameCity = Console.ReadLine();
+                CityNumArray[i].Citynearly = int.Parse(Console.ReadLine());
+                CityNumArray[i].CityNearNum = int.Parse(Console.ReadLine());
+
+            }
+        }
+    }
+}
